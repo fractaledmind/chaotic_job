044ceb42967c47f97e27990eb50998c9b0f8f42e
Stephen Margheim <stephen.margheim@gmail.com>
2024-11-07 11:08:02 +0100

    Add some tests for the Performer module

--- test/test_chaotic_job.rb
+++ test/test_chaotic_job.rb
@@ -2,12 +2,63 @@
 
 require "test_helper"
 
-class TestChaoticJob < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ChaoticJob::VERSION
+class TestChaoticJob < ActiveJob::TestCase
+  include ChaoticJob::Helpers
+
+  test "performing a simple job" do
+    class Job1 < ActiveJob::Base
+      def perform
+        step_1
+        step_2
+      end
+
+      def step_1; ChaoticJob::Journal.log; end
+      def step_2; ChaoticJob::Journal.log; end
+    end
+
+    Job1.perform_later
+    perform_all
+
+    assert_equal 2, ChaoticJob::Journal.total
   end
 
-  def test_it_does_something_useful
-    assert false
+  test "performing a job that schedules another job" do
+    class Job2 < ActiveJob::Base
+      class ChildJob < ActiveJob::Base
+        def perform
+          ChaoticJob::Journal.log(scope: :child)
+        end
+      end
+
+      retry_on StandardError
+
+      def perform
+        step_1
+        step_2
+        step_3
+      end
+
+      def step_1; ChaoticJob::Journal.log(scope: :parent); end
+      def step_2; ChildJob.set(wait: 1.week).perform_later; end
+      def step_3
+        raise StandardError if executions == 1
+        ChaoticJob::Journal.log(scope: :parent)
+      end
+    end
+
+    Job2.perform_later
+    perform_all_within(4.seconds)
+
+    assert_equal 2, enqueued_jobs.size
+    assert_equal 2, performed_jobs.size
+    assert_equal 3, ChaoticJob::Journal.total(scope: :parent)
+    assert_equal 0, ChaoticJob::Journal.total(scope: :child)
+
+    perform_all_after(7.days)
+
+    assert_equal 0, enqueued_jobs.size
+    assert_equal 4, performed_jobs.size
+    assert_equal 3, ChaoticJob::Journal.total(scope: :parent)
+    assert_equal 2, ChaoticJob::Journal.total(scope: :child)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,4 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "chaotic_job"
 
 require "minitest/autorun"
+require "active_job"
