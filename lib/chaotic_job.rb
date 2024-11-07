935ada1cfb239914d1f5473d1e12ace0bd233c7a
Stephen Margheim <stephen.margheim@gmail.com>
2024-11-07 11:07:17 +0100

    Require the sub-modules

--- lib/chaotic_job.rb
+++ lib/chaotic_job.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "chaotic_job/version"
+require_relative "chaotic_job/journal"
+require_relative "chaotic_job/performer"
+require_relative "chaotic_job/glitch"
+require_relative "chaotic_job/scenario"
+require_relative "chaotic_job/simulation"
 
 module ChaoticJob
   class RetryableError < StandardError; end
