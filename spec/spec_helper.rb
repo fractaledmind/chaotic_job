913d3cfd5faeac4fbf96db10f861cf7cdce9c174
Stephen Margheim <stephen.margheim@gmail.com>
2025-08-14 23:44:05 +0200

    Add RSpec tests for Simulation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,37 @@
 require "bundler/setup"
+require "active_job"
+require "chaotic_job"
 
-RSpec.configure do |config|
-  config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = ".rspec_status"
-  config.disable_monkey_patching!
-  config.order = :defined
-  config.expect_with(:rspec) { |c| c.syntax = :expect }
-  config.raise_errors_for_deprecations!
+class TestJob < ActiveJob::Base
+  def perform(value = :performed, recursions: nil, wait: nil, total: nil)
+    ChaoticJob.log_to_journal!(recursions || value)
+
+    if recursions && recursions > 0
+      total ||= recursions
+      delay = wait ? (wait * (total - recursions + 1)) : 0
+
+      TestJob
+        .set(wait: delay)
+        .perform_later(value, recursions: recursions - 1, wait: wait, total: total)
+    end
+  end
 end
 
-require "chaotic_job"
+ActiveJob::Base.queue_adapter = :test
+ActiveJob::Base.logger = Logger.new(ENV["LOG"].present? ? $stdout : IO::NULL)
+$VERBOSE = ENV["LOG"].present? ? true : nil
+
+RSpec.configure do |config|
+  config.include ActiveJob::TestHelper
+
+  config.before(:each) do
+    clear_enqueued_jobs
+    clear_performed_jobs
+    ChaoticJob::Journal.reset! if defined?(ChaoticJob::Journal)
+  end
+
+  config.after(:each) do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+end
